d7958573db298cf6b38079303f69a5b95e6aabf2
tarassss1 <143933998+tarassss1@users.noreply.github.com>
2024-06-02 15:28:55 +0300

    Add files via upload

---
+++ launch.py
@@ -0,0 +1,20 @@
+import pygame
+import main
+pygame.init()
+
+window = pygame.display.set_mode((600,400))
+clock = pygame.time.Clock()
+player = main.Player((150,150))
+game = True
+while game:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+
+    player.events(event)
+    window.fill((0,0,0))
+    window.blit(player.image, player.rect)
+
+    clock.tick(30)
+    pygame.display.update()
